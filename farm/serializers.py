e78da8d669761865b0661c925a3ebec9772fcfe1
Ahas <ahasraajvv@gmail.com>
2026-09-01 19:53:46 +0530

    feat: add serializers, migrations, and .gitignore
    - Created ModelSerializer for all 6 farm models (Batch, BatchExpense, BatchSale, MortalityRate, InfraExpense, CustomerDetail). - Added initial migration (0001_initial.py) for farm app. - Registered farm app in INSTALLED_APPS. - Added .gitignore to exclude __pycache__, db.sqlite3, and venv.

---
+++ farm/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from .models import Batch, BatchExpense, MortalityRate, CustomerDetail, BatchSale, InfraExpense
+
+class BatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Batch
+        fields = '__all__'  
+
+
+class BatchExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BatchExpense
+        fields = '__all__'
+
+class MortalityRateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MortalityRate
+        fields = '__all__'
+
+class InfraExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InfraExpense
+        fields = '__all__'
+
+class CustomerDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerDetail
+        fields = '__all__'
+
+    
+class BatchSaleSerializer(serializers.ModelSerializer):
+    batch = BatchSerializer()
+    customer = CustomerDetailSerializer()
+    class Meta:
+        model = BatchSale
+        fields = '__all__'
+
+
+class BatchSaleDetailSerializer(serializers.ModelSerializer):
+    batch = BatchSerializer()
+    customer = CustomerDetailSerializer()
+
+    class Meta:
+        model = BatchSale
+        fields = '__all__'
